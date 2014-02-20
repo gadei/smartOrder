22f67093a9ddcdd01f68205e258098e7e8cb41b3
Marco Steger <marco.st1987@gmail.com>
2014-02-20 19:08:38 +0100

    ...

--- smartOrderClient/gen/smart/order/client/R.java
+++ smartOrderClient/gen/smart/order/client/R.java
@@ -30,26 +30,37 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Bar=0x7f080009;
-        public static final int Tisch1=0x7f080007;
-        public static final int Tisch2=0x7f080008;
-        public static final int Tisch3=0x7f08000c;
-        public static final int Tisch4=0x7f08000b;
-        public static final int Tisch5=0x7f08000a;
-        public static final int button1=0x7f08000d;
-        public static final int button2=0x7f08000f;
+        public static final int Bar=0x7f080011;
+        public static final int Tisch1=0x7f08000f;
+        public static final int Tisch2=0x7f080010;
+        public static final int Tisch3=0x7f080014;
+        public static final int Tisch4=0x7f080013;
+        public static final int Tisch5=0x7f080012;
+        public static final int button1=0x7f080015;
+        public static final int button2=0x7f080017;
         public static final int connectClientButton=0x7f080001;
-        public static final int disconnectClientButton=0x7f080006;
+        public static final int detailFragment=0x7f080004;
+        public static final int detailsText=0x7f080006;
+        public static final int disconnectClientButton=0x7f08000e;
         public static final int fullscreen_content=0x7f080002;
         public static final int fullscreen_content_controls=0x7f080000;
-        public static final int fullscreen_content_controls_smartOrder=0x7f080005;
-        public static final int fullscreen_content_smartOrder=0x7f080003;
-        public static final int inside_frame=0x7f080004;
-        public static final int outside_frame=0x7f08000e;
+        public static final int fullscreen_content_controls_smartOrder=0x7f08000d;
+        public static final int fullscreen_content_smartOrder=0x7f08000b;
+        public static final int inside_frame=0x7f08000c;
+        public static final int listFragment=0x7f080003;
+        public static final int listView1=0x7f080008;
+        public static final int listView2=0x7f08000a;
+        public static final int outside_frame=0x7f080016;
+        public static final int textView1=0x7f080005;
+        public static final int textView2=0x7f080009;
+        public static final int textView3=0x7f080007;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
-        public static final int smart_order_activity=0x7f030001;
+        public static final int order_activity=0x7f030001;
+        public static final int order_fragment_detail=0x7f030002;
+        public static final int order_fragment_list=0x7f030003;
+        public static final int smart_order_activity=0x7f030004;
     }
     public static final class string {
         public static final int SmartOrderActivity_view_inside=0x7f050009;
